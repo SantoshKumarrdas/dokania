dea10319938e49d4a9c2c64e66639fc121f656cf
SantoshKumarrdas <santoshy.das2004@gmail.com>
2025-10-04 15:41:00 +0530

    bug fix

--- client/src/components/layouts/Footer.jsx
+++ client/src/components/layouts/Footer.jsx
@@ -109,7 +109,7 @@ const Footer = () => {
             <div className="border-t border-gray-300">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
                     <p className="text-center text-sm text-gray-600">
-                        Copyright © 2025 Dokania Tech Solutions. | Website Designed & Developed by Amit Keshari
+                        Copyright © 2025 Dokania Tech Solutions. | Website Designed & Developed by Santosh Kumar
                     </p>
                 </div>
             </div>
