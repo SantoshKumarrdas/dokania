1c2663d07dbd50d5619418e6265b324030e9a9a0
SantoshKumarrdas <santoshy.das2004@gmail.com>
2025-10-04 14:36:53 +0530

    bug fix

--- client/src/app/403/page.js
+++ client/src/app/403/page.js
@@ -3,7 +3,7 @@ export default function Forbidden() {
         <div className="min-h-[50vh] flex items-center justify-center">
             <div className="text-center">
                 <h1 className="text-4xl font-bold mb-2">403 - Forbidden</h1>
-                <p className="text-gray-600">You don't have permission to access this page.</p>
+                <p className="text-gray-600">You don&apos;t have permission to access this page.</p>
             </div>
         </div>
     );
